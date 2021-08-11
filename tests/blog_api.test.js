6b98f791854085d7e50a0024766c0037e76773d3
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-08-11 11:15:53 -0700

    fullstack exercises 4.8 test for adding blog

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,7 +5,7 @@ const app = require('../index');
 const api = supertest(app);
 
 
-beforeEach(async () => {
+beforeEach(async() => {
   await Blog.deleteMany({});
   for (index = 0; index < helper.initialBlogs.length; index += 1) {
     const blog = new Blog(helper.initialBlogs[index]);
@@ -13,19 +13,40 @@ beforeEach(async () => {
   }
 });
 
-test('blogs returned as json', async () => {
+test('blogs returned as json', async() => {
   const request = await api.get('/api/blogs')
                             .expect(200)
                             .expect('Content-Type', /application\/json/);
 })
 
-test('blogs returned the corrent number of blog posts', async () => {
+test('blogs returned the corrent number of blog posts', async() => {
   const request = await api.get('/api/blogs')
   const blogs = request.body;
   expect(blogs).toHaveLength(helper.initialBlogs.length)
 });
 
-test('blog identifier attribute is id', async () => {
+test('blog identifier attribute is id', async() => {
   const request = await api.get('/api/blogs')
   expect(request.body[0]).toBeDefined();
+});
+
+test('valid blog is added', async() => {
+  const newBlog = {
+    title: "Blog",
+    author: "author",
+    url: "url",
+    likes: 23
+  }
+
+  await api.post('/api/blogs')
+           .send(newBlog)
+           .expect(201)
+           .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+  const blogs = response.body;
+  expect(blogs).toHaveLength(helper.initialBlogs.length + 1);
+
+  const likes = blogs.map(({likes}) => likes)
+  expect(likes).toContain(23);
 });
