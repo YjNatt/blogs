08ac4e08d4d8836d40abb66c01fac9d0dd35d940
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-08-10 17:23:29 -0700

    fullstack exercises 4.2 separate the application into modules

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const blogRouter = require('./controllers/blogs');
 
 const mongoURL = config.MONGODB_URI
 mongoose.connect(mongoURL, {
@@ -24,19 +16,7 @@ mongoose.connect(mongoURL, {
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-      .then(blogs => response.json(blogs));
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-  blog.save()
-      .then(result => {
-        response.status(201).json(result);
-      });
-});
+app.use('/api/blogs', blogRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
