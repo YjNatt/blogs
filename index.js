1dafa7049c1221d2ca179d5446da9f8ca38ad69b
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-08-11 20:44:26 -0700

    fulstack exercises 4.18 implement token authentication for users

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 
 const mongoURL = config.MONGODB_URI
@@ -23,6 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
